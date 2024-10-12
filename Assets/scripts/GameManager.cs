295d76a1fd6dfeb383424223833634e71864516a
Ville Lindberg <ville.p.lindberg@gmail.com>
2024-10-13 02:33:15 +0300

    minor fix

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : Singleton<GameManager>
     [SerializeField]
     private InGameUI inGameUI;
 
+    private Daytimes _daytime;
+
     public Daytimes Daytime { get => _daytime; set => _daytime = value; }
 
     public GameManager()
