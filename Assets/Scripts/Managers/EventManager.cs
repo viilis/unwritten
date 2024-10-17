b5d9c00ae5920cd39b20f5a5a11c568b0e6cdbc5
Ville Lindberg <ville.p.lindberg@gmail.com>
2024-10-17 15:53:01 +0300

    sanity change event

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -9,7 +9,41 @@ public class EventManager : Singleton<EventManager>
     [SerializeField]
     private float eventInterval = 5f; // 5 seconds
 
+    [SerializeField]
+    private float level1Threshold = 70f;
+
+    [SerializeField]
+    private float level2Threshold = 50f;
+
+    [SerializeField]
+    private float level3Threshold = 20f;
+
     private float _timer;
+
+    private void OnEnable()
+    {
+        PlayerSanity.OnSanityChange += OnSanityChange;
+    }
+
+    private void OnSanityChange(float currentSanity)
+    {
+        if (currentSanity <= level1Threshold)
+        {
+            Debug.Log("Level 1.");
+            OnParanormalObjMovementEvent?.Invoke();
+        }
+
+        if (currentSanity <= level2Threshold)
+        {
+            Debug.Log("Level 2.");
+            OnParanormalElectronicsEvent?.Invoke();
+        }
+
+        if (currentSanity <= level3Threshold)
+        {
+            Debug.Log("Level 3.");
+        }
+    }
 
     private void Start()
     {
@@ -24,9 +58,12 @@ public class EventManager : Singleton<EventManager>
         if (_timer <= 0)
         {
             // Call action e.g OnParanormalElectronicsEvent?.Invoke();
-            OnParanormalElectronicsEvent?.Invoke();
-            OnParanormalObjMovementEvent?.Invoke();
             _timer = eventInterval;
         }
+    }
+
+    private void OnDisable()
+    {
+        PlayerSanity.OnSanityChange -= OnSanityChange;
     }
 }
